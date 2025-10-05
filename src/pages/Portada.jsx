e91da4c3266897af5bae2ebd3cc1a0ffd2c34f6e
eugelu11 <34776849+eugelu11@users.noreply.github.com>
2025-10-05 18:00:44 -0300

    portada 1er commit

--- src/pages/Portada.jsx
+++ src/pages/Portada.jsx
@@ -1,9 +1,31 @@
+import styles from "./portada.module.css";
+import Button from "../components/Button/Button";
+
 function Portada() {
+  const containerStyle = {
+    display: "flex",
+    flexWrap: "wrap",
+    gap: "1rem",
+    justifyContent: "center",
+  };
+
     return (
         <>
-            <h2>Portada</h2>
-            <p>Hola esta es la portada</p>
-        </>
+              <h1>Grupo 2 · TP01</h1>
+        <div class="fotoMain">
+            <div class="Titulo">GRUPO DOS</div>
+        </div>
+
+
+        <h2>¿Quiénes somos?</h2>
+
+        <ul id="listaMain">
+            <li>Somos un grupo de compañeros juntos desde 2023</li>
+            <li>Buscamos constantemente mejorar nuestras habilidades de programación ayudandonos mutuamente</li>
+            <li>Todos venimos de ramas muy diferentes y aportamos diferentes habilidades</li>
+        </ul>
+
+          </>
     );
 }
 
