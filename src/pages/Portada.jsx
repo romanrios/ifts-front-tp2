003ac0aad60fbe23afdf7ee5b0a134dfb63b9a56
eugelu11 <34776849+eugelu11@users.noreply.github.com>
2025-10-07 21:19:27 -0300

    portada terminada

--- src/pages/Portada.jsx
+++ src/pages/Portada.jsx
@@ -9,24 +9,29 @@ function Portada() {
     justifyContent: "center",
   };
 
-    return (
-        <>
-              <h1>Grupo 2 · TP01</h1>
-        <div class="fotoMain">
-            <div class="Titulo">GRUPO DOS</div>
-        </div>
+  return (
+    <>
+      <h1>Grupo 2 · TP01</h1>
 
+      <div className={styles.fotoMain}>
+        <div className={styles.Titulo}>GRUPO DOS</div>
+      </div>
 
-        <h2>¿Quiénes somos?</h2>
+      <h2>¿Quiénes somos?</h2>
 
-        <ul id="listaMain">
-            <li>Somos un grupo de compañeros juntos desde 2023</li>
-            <li>Buscamos constantemente mejorar nuestras habilidades de programación ayudandonos mutuamente</li>
-            <li>Todos venimos de ramas muy diferentes y aportamos diferentes habilidades</li>
-        </ul>
-
-          </>
-    );
+      <ul className={styles.listaMain}>
+        <li>Somos un grupo de compañeros juntos desde 2023</li>
+        <li>
+          Buscamos constantemente mejorar nuestras habilidades de programación
+          ayudándonos mutuamente
+        </li>
+        <li>
+          Todos venimos de ramas muy diferentes y aportamos diferentes
+          habilidades
+        </li>
+      </ul>
+    </>
+  );
 }
 
 export default Portada;
